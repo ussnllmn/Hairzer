0526c51aaf1e1f734a61fc5647f1e7514d27f6ec
Songvut Nakrong <songvut.nakrong@gmail.com>
2020-10-31 15:26:42 +0700

    Update comment

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
+<!--/src/App.vue-->
+
 <template>
   <div id="app">
     <Navbar></Navbar>
@@ -9,19 +11,19 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+  import Navbar from './components/Navbar'
+  import Footer from './components/Footer'
 
-export default {
-  components: {
-    Navbar,
-    Footer
+  export default {
+    components: {
+      Navbar,
+      Footer
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  color: #495057;
-}
+  #app {
+    color: #495057;
+  }
 </style>
