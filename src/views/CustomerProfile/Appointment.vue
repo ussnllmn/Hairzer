1fd0b1cd31fe7016dbeb843ab59a5a8eecb5f638
Songvut Nakrong <songvut.nakrong@gmail.com>
2020-11-09 00:28:41 +0700

    [ADD] appointment page

--- src/views/CustomerProfile/Appointment.vue
+++ src/views/CustomerProfile/Appointment.vue
@@ -3,24 +3,61 @@
         <h1>จัดการนัดหมาย</h1>
         <div class="appointmentBox p-2">
             <!--Title-->
-            <b-row>
-                <b-col>นัดหมาย</b-col>
-                <b-col>ว/ด/ป</b-col>
-                <b-col>เวลา</b-col>
-                <b-col>สถานที่</b-col>
-                <b-col>ช่างตัดผม</b-col>
-                <b-col>บริการ</b-col>
-                <b-col>สถานะ</b-col>
-                <b-col>การจัดการ</b-col>
-            </b-row>
+            <b-table striped hover :items="items" :fields="fields">
+                <template #cell(การจัดการ)>
+                    <b-button variant="outline-primary" size="sm" class="mb-2" style="width: 100%;">รายละเอียด</b-button> <br>
+                    <b-button variant="outline-success" size="sm" class="mb-2" style="width: 100%;">ยืนยัน</b-button> <br>
+                    <b-button variant="outline-danger" size="sm" style="width: 100%;">รายละเอียด</b-button> <br>
+                </template>
+            </b-table>
+            
         </div>
     </div>
 </template>
 
-<style scoped>
-    .appointmentBox {
-        border: 1px solid #CED4DA;
-        border-radius: 5px;
-        color: #495057;
+<script>
+  export default {
+    data() {
+        return {
+            fields: [
+                { key:'appointmentID', sortable: true },
+                { key:'Date', sortable: true },
+                { key:'Time', sortable: true },
+                { key:'location', sortable: true },
+                { key:'barber', sortable: true },
+                { key:'status', sortable: true },
+                { key:'การจัดการ' }
+            ],
+            items: [
+                { 
+                    appointmentID: '000021', 
+                    Date: '2020-11-19', 
+                    Time: '20:00:00', 
+                    location: 'ร้านตัดผม 1', 
+                    barber: 'ช่างตัดผม 1', 
+                    service: ['บริการ 1', 'บริการ 2'], 
+                    status: 'waiting' 
+                },
+                { 
+                    appointmentID: '000521', 
+                    Date: '2020-12-11', 
+                    Time: '10:50:00', 
+                    location: 'ร้านตัดผม 2', 
+                    barber: 'ช่างตัดผม 4', 
+                    service: ['บริการ 2', 'บริการ 3', 'บริการ 4'], 
+                    status: 'waiting' 
+                },
+                { 
+                    appointmentID: '000651', 
+                    Date: '2020-12-22', 
+                    Time: '15:40:00', 
+                    location: 'ร้านตัดผม 3', 
+                    barber: 'ช่างตัดผม 2', 
+                    service: ['บริการ 1', 'บริการ 3', 'บริการ 4'], 
+                    status: 'waiting' 
+                },
+            ]
+        }
     }
-</style>
+  }
+</script>
